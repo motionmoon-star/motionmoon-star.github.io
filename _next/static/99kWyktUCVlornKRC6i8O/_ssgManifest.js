8a3e4f4dc4df9143a93c07e3744450c8682b1578
motionmoon-star <motion.moon@gmail.com>
2026-09-02 23:24:39 +0200

    Vitrine statique BLANLUX

---
+++ _next/static/99kWyktUCVlornKRC6i8O/_ssgManifest.js
@@ -0,0 +1,1 @@
+self.__SSG_MANIFEST=new Set(["\u002Factualites\u002F[slug]","\u002Fproduits\u002F[slug]","\u002Fproduits\u002F[slug]\u002F[produit]"]);self.__SSG_MANIFEST_CB&&self.__SSG_MANIFEST_CB()
